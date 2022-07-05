1039c09a1957e6e2ae53a906cd664a944a0c6a51
Yixuan-LULU <54480938+Yixuan-LULU@users.noreply.github.com>
2022-07-04 20:57:07 -0700

    Arith, Array and Variables

---
+++ Day 1/Arithmetics/Arithmetics.java
@@ -0,0 +1,7 @@
+public class Arithmetics {
+    public static void main(String[] args){
+        //6 multiplied by 1234124 divided by 11345 minus 890809 plus 90800
+        double res = (6.0*1234124.0/112345.0-890809+90800)%980098;
+        System.out.print(res);
+    }    
+}
---
+++ Day 1/Arrays/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] info = {"Tech", "Rohin", "Optix 3749", "Boe Jiden"};
+        for (int i = 0; i < 4; i++){
+            System.out.print(info[i] + " ");
+        }
+    }    
+}
